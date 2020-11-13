a5ed5d43b2a18f5f9c998647a21dcdce0b0ba170
Shruthi Sagar CR <sagar.s@digicollect.com>
2020-11-13 07:50:28 +0530

    #added app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  import './App.css';
+import React, {Component} from 'react';
+import  {CardList} from './components/card-list/card-list.component'
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      users: [],
+      searchField: "",
+      title:''
+    };
+    // this.handleChange = this.handleChange.bind(this)
+  }
+  componentDidMount(){
+    fetch('http://jsonplaceholder.typicode.com/users').then(response=>response.json()).then(
+      users=>this.setState({users:users})
+    )
+  }
+  handleChange = (e) =>{
+    this.setState({searchField:e.target.value, title:e.target.value})
+  }
+  render() {
+    const {users, searchField, title} = this.state;
+    const filteredUsers = users.filter(user=>user.name.toLowerCase().includes(searchField.toLowerCase()))
+    return (
+      <div className="App">
+        
+        <input type="search" onChange={this.handleChange}/>
+        <h1>{title}</h1>
+        <CardList users={filteredUsers}>
+        </CardList>
+        
+      </div>
+    );    
+  }
 }
 
 export default App;
